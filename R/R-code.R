3f05f444ddbb6bca03bc4db3251f669808606c86
KateM3244 <mullins.k38@gmail.com>
2024-12-19 12:25:21 +0000

    first function

---
+++ R/R-code.R
@@ -0,0 +1,28 @@
+library(dplyr)
+library(janitor)
+data <- read_csv("R/Advanced r programming dataset.csv")
+
+data<- as.data.frame(data)
+
+
+utils::globalVariables(c("x", "y", "SPAD", "chlorophyll", "stomatal", "leaf thickness","Leaf number","Tree ID", "Pot ID", "Culture", "Temperature"))
+
+## Function 1 : Making column names the same and removing any NA values
+
+tidy_data <- function(data) {
+
+  if (!is.data.frame(data)) {
+    stop("Input must be a data frame")
+  }
+
+  tidied_data <- data %>%
+    na.omit(data) %>%
+    janitor::clean_names(case = "snake") ## changes all column names to snake_case (lowercase and spaces replaced with)
+
+  return(tidied_data)
+}
+
+
+cleaned_Data <-tidy_data(data)
+
+devtools::check()
