fb4810446e90d0d3fbd7eadb171c1aba79e5265e
Erin-Weadick <erin.weadick.2022@mumail.ie>
2025-01-16 18:32:44 +0000

    roxygenm doc and small change to function 1

--- R/R-code.R
+++ R/R-code.R
@@ -5,23 +5,38 @@ library(roxygen2)
 library(ggplot2)
 
 data <- read_csv("R/Advanced r programming dataset.csv")
-
 data<- as.data.frame(data)
-
 
 utils::globalVariables(c("x", "y", "SPAD", "chlorophyll", "stomatal", "leaf thickness","Leaf number","Tree ID", "Pot ID", "Culture", "Temperature"))
 
 ## Function 1 : Making column names the same and removing any NA values
 
-#' Title
+#' Tidy data for use in other functions
 #'
 #' @param data
+#' @param case The case to which the data column names will be changed to. The case options are \code{"snake"}
+#' (the default), \code{"upper_camel"}, \code{"lower_camel"}, or \code{"title"}
 #'
-#' @return
+#' @return An object of class \code{"tidy_data"} which is tidied by omitting NA values and changing column names to be of the same case.
 #' @export
+#' @author Erin Weadick - <\email{erin.weadick.2022@@mumail.ie}>
+#' @author Kate Mullins - <\email{kate.mullins.2022@@mumail.ie}>
+#' @author Daniel McKean - <\email{daniel.mckean.2022@@mumail.ie}>
+#' @author Laurynas Jonikas - <\email{laurynas.jonikas.2022@@mumail.ie}>
 #'
 #' @examples
-tidy_data <- function(data) {
+#' tidy_data <- function(data) {
+#' if (!is.data.frame(data)){
+#' stop("Input must be a data frame")
+#' }
+#'
+#' tidied_data <- data %>%
+#' na.omit(data) %>%
+#' clean_names(case = case)
+#' }
+#'
+#' new_data <- tidy_data(data, case = "upper_camel")
+tidy_data <- function(data, case = "snake") {
 
   if (!is.data.frame(data)) {
     stop("Input must be a data frame")
@@ -29,29 +44,49 @@ tidy_data <- function(data) {
 
   tidied_data <- data %>%
     na.omit(data) %>%
-    clean_names(case = "snake") ## changes all column names to snake_case (lowercase and spaces replaced with)
+    clean_names(case = case)
 
   return(tidied_data)
 }
+?clean_names
 
-
-cleaned_Data <-tidy_data(data)
+cleaned_Data <-tidy_data(data, "upper_camel")
 
 
 ## Function 2: Making this dataset available as part of this R package
 
-#' Title
+#' Load in and save plant data for use with the package.
+#'
+#' A function to save the data from the package as an R object to be used
+#' with the other functions inside the package. It is saved using the
+#' 'usethis :: use_data' function
 #'
 #' @param data
 #'
-#' @return
+#' @return An object of class \code{"data"} which is a list of \code{\link[tibble]{tibble}}s which
+#' contains plant stress responses for species in ambient and warm temperature conditions.
+#'
+#' @details
+#' The function saves the data with 'compress = "xz"' to reduce the file size
+#' and 'overwrite = TRUE' to allow it to overwrite any existing data in the
+#' environment with the same name.
+#'
 #' @export
+#' @author Erin Weadick - <\email{erin.weadick.2022@@mumail.ie}>
+#' @author Kate Mullins - <\email{kate.mullins.2022@@mumail.ie}>
+#' @author Daniel McKean - <\email{daniel.mckean.2022@@mumail.ie}>
+#' @author Laurynas Jonikas - <\email{laurynas.jonikas.2022@@mumail.ie}>
+#'
+#' @importFrom usethis use_data
 #'
 #' @examples
+#' \dontrun{
+#' clean_data <- data.frame(x = 1:10, y = rnorm(10))
+#' load_Data(clean_Data)
+#' }
+#'
 load_data <- function(data){
-
   usethis::use_data(data, compress="xz",overwrite = TRUE)
-
 }
 
 load_data(cleaned_Data)
@@ -68,10 +103,18 @@ load_data(cleaned_Data)
 #' @param ... Other arguments passed to the underlying \code{ggplot2} functions.
 #'
 #' @return A \code{ggplot} object.
+#'
 #' @export
+#' @author Erin Weadick - <\email{erin.weadick.2022@@mumail.ie}>
+#' @author Kate Mullins - <\email{kate.mullins.2022@@mumail.ie}>
+#' @author Daniel McKean - <\email{daniel.mckean.2022@@mumail.ie}>
+#' @author Laurynas Jonikas - <\email{laurynas.jonikas.2022@@mumail.ie}>
+#'
+#' @importFrom ggplot2 "ggplot"
 #'
 #' @examples
-#' plot.tidy_data(cleaned_Data, "spad", "chlorophyll")
+#'
+#' plot.tidy_data(cleaned_Data, "spad", "chlorophyll", )
 plot.tidy_data <- function(object, x, y, ...) {
   if (missing(x) || missing(y)) {
     stop("You must specify both x and y for the plot.")
